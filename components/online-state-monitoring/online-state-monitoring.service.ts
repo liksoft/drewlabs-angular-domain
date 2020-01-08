9542bf07ae4721373ce13ccd34f27d014d48c469
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-01-08 22:30:12 +0000

    Added online status component and updated int-tel-input component fixing bug related to custom validator

---
+++ components/online-state-monitoring/online-state-monitoring.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ConnectionStatus } from './connection-status';
+import { WindowRef } from '../../utils/window-ref';
+
+enum EventType {
+  ONLINE_EVENT = 'online',
+  OFFLINE_EVENT = 'offline'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OnlineStateMonitoringService implements OnDestroy {
+
+  public readonly connectionStatus: BehaviorSubject<number> = new BehaviorSubject(1);
+
+  constructor(private windowRef: WindowRef) { }
+
+  registerToConnectionStates() {
+    this.windowRef.nativeWindow.addEventListener(EventType.ONLINE_EVENT, () => {
+      this.connectionStatus.next(ConnectionStatus.ONLINE);
+    });
+
+    this.windowRef.nativeWindow.addEventListener(EventType.OFFLINE_EVENT, () => {
+      this.connectionStatus.next(ConnectionStatus.OFFLINE);
+    });
+  }
+
+  ngOnDestroy() {
+    this.windowRef.nativeWindow.removeEventListener(EventType.ONLINE_EVENT, () => {});
+    this.windowRef.nativeWindow.removeEventListener(EventType.OFFLINE_EVENT, () => {});
+    this.connectionStatus.complete();
+  }
+}
