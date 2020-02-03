1757e53dfcffc352142dce0514091c46a0859af7
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-02-03 17:01:35 +0000

    Removes unnecessary component and modules

--- components/app-breadcrumb/index.ts
+++
@@ -1,2 +0,0 @@
-export { AppBreadcrumbComponent } from './app-breadcrumb.component';
-export {BreadCrumbUIState, BreadCrumbStore} from './breadcrumb-store';
--- components/core-components.module.ts
+++ components/core-components.module.ts
@@ -1,11 +1,7 @@
 import { DropzoneModule } from './dropzone';
-import { AppBreadcrumbComponent, BreadCrumbStore } from './app-breadcrumb';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { LoaderModule } from './loader/loader.module';
-import { AppLoaderModalComponent } from './app-loader-modal/app-loader-modal.component';
-import { PaginatorComponent } from './paginator/paginator.component';
 import { DynamicComponentService } from './services/dynamic-component-resolver.service';
 import { NgDataTableModule } from './ng-data-table/ng-data-table.module';
 import { AppAlertModule } from './app-alert';
@@ -18,7 +14,6 @@ import { OnlineStateMonitoringComponent } from './online-state-monitoring/online
   imports: [
     CommonModule,
     RouterModule,
-    LoaderModule,
     NgDataTableModule,
     AppAlertModule.forRoot(),
     ProgressBarModule,
@@ -27,10 +22,6 @@ import { OnlineStateMonitoringComponent } from './online-state-monitoring/online
     DynamicFormControlModule.forRoot()
   ],
   exports: [
-    AppBreadcrumbComponent,
-    AppLoaderModalComponent,
-    LoaderModule,
-    PaginatorComponent,
     ProgressBarModule,
     NgDataTableModule,
     AppAlertModule,
@@ -39,13 +30,9 @@ import { OnlineStateMonitoringComponent } from './online-state-monitoring/online
     OnlineStateMonitoringComponent,
   ],
   declarations: [
-    AppBreadcrumbComponent,
-    AppLoaderModalComponent,
-    PaginatorComponent,
     OnlineStateMonitoringComponent,
   ],
   providers: [
-    BreadCrumbStore
   ]
 })
 export class CoreComponentModule {
