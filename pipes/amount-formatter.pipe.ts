747a50b3ecffa5fb9dfeef98663e0650ff13fafb
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-03-09 08:33:51 +0000

    Updated amount formater pipe

--- pipes/amount-formatter.pipe.ts
+++ pipes/amount-formatter.pipe.ts
@@ -1,11 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { AmountFormatter } from '../utils/formatters';
+import { isDefined } from '../utils/type-utils';
 
 @Pipe({
   name: 'amountFormatter'
 })
 export class AmountFormatterPipe implements PipeTransform {
   transform(value: any, decimal: any | number = 0, seperator: string = ' '): any {
+    if (!isDefined(value)) {
+      return '0.00';
+    }
     return AmountFormatter.formatBalance(value, decimal, seperator);
   }
 }
