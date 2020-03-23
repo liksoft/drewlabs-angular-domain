0e11f050b8415477951672b0473730c55f37e07f
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-03-23 08:13:09 +0000

    Fixed issue with empty string for date input blur event

--- components/dynamic-inputs/dynamic-form-control/dynamic-form-control.component.ts
+++ components/dynamic-inputs/dynamic-form-control/dynamic-form-control.component.ts
@@ -296,8 +296,12 @@ export class DynamicFormControlComponent implements OnInit, OnDestroy {
   }
 
   onDateInputBlur(lang: string = 'fr') {
+    const value: string = this.control.value as string;
+    if ((value === '') || (value.length === 0)) {
+      this.control.setValue(null);
+      return;
+    }
     if (isDefined(this.control.value)) {
-      const value: string = this.control.value as string;
       if (!value.match(/(0*([1-9]|1[0-2]))\/(0*([1-9]|[12][0-9]|3[01]))\/\d{4}/)) {
         // tslint:disable-next-line: max-line-length
         const output: { days: string, month: string, year: string } = { days: value.substr(0, 2), month: value.substr(2, 2), year: value.substr(4) };
