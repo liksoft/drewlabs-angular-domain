039cdee02f959b0292c5f796bd1dd0971f535311
benji <benbosspayaro@gmail.com>
2020-04-30 12:19:09 +0000

    ok

--- auth/core/auth.service.ts
+++ auth/core/auth.service.ts
@@ -1,6 +1,5 @@
 import { AuthUser } from '../contracts';
 import { Injectable, Inject } from '@angular/core';
-import 'core-js/es/object';
 import 'rxjs/operators/map';
 import {
   ServerResponseKeys,
