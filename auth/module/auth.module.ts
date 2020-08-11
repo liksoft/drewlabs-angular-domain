2b8af914896d6403bd3ac3c1bbd12db1a869428b
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-08-11 22:56:12 +0000

    Updated auth.module.ts - fixed issue with imports of department, permission and roles servcies

--- auth/module/auth.module.ts
+++ auth/module/auth.module.ts
@@ -3,10 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AuthGuardService } from '../core/auth-guard.service';
 import { AuthService } from '../core/auth.service';
 import { AuthUserService } from '../core/user.service';
-import { Roleservice } from '../core/services/roles.service';
 import { ApplicationUsersService } from '../core/services/users.service';
-import { PermissionsService } from '../core/services/permissions.service';
-import { Departmentservice } from '../core/services/department.service';
 
 @NgModule({
   imports: [
@@ -22,11 +19,8 @@ export class AuthModule {
         AuthGuardService,
         AuthService,
         AuthUserService,
-        PermissionsService,
-        Roleservice,
-        ApplicationUsersService,
-        Departmentservice
-      ],
+        ApplicationUsersService
+      ]
     };
   }
 }
