40f59f18659be16fabc4938746be7edb83555a9c
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-09-20 22:25:19 +0000

    Added sorting to paginator

--- pagination/helpers.ts
+++ pagination/helpers.ts
@@ -1,16 +1,48 @@
-import { ClrDatagridStateInterface } from '@clr/angular';
-import { isArray } from '../utils/types/type-utils';
+import { ClrDatagridStateInterface } from "@clr/angular";
+import { isArray } from "../utils/types/type-utils";
 
-export const mapPaginatorStateWith = (params: { [index: string]: any }[] | { [index: string]: any } = []) =>
+export const mapPaginatorStateWith =
+  (params: { [index: string]: any }[] | { [index: string]: any } = []) =>
   (state: ClrDatagridStateInterface) => {
+    // 'order' => 'desc', 'by' => 'updated_at'
     const filters: { [prop: string]: any[] } = {};
+    let query: { [prop: string]: any } = {};
+    //#region Add sort filters to the list of query filters
+    if (state?.sort) {
+      query = {
+        _query: JSON.stringify({
+          orderBy: {
+            order: state?.sort?.reverse ? "DESC" : "ASC",
+            by: state?.sort?.by || "updated_at",
+          },
+        }),
+      };
+    } else {
+      query = {
+        _query: JSON.stringify({
+          orderBy: {
+            order: "DESC",
+            by: "updated_at",
+          },
+        }),
+      };
+    }
+    //#endregion Add sort filters to the list of query filters
     if (state.filters) {
       for (const filter of state.filters) {
-        const { property, value } = filter as { property: string, value: string };
+        const { property, value } = filter as {
+          property: string;
+          value: string;
+        };
         filters[property] = [value];
       }
     }
-    let currenState = Object.assign(filters, { page: state?.page?.current || 1, per_page: state?.page?.size || 20 });
+    let currenState = {
+      ...filters,
+      page: state?.page?.current || 1,
+      per_page: state?.page?.size || 20,
+      ...query,
+    };
     if (isArray(params)) {
       params.map((p: object) => {
         currenState = { ...currenState, ...p };
