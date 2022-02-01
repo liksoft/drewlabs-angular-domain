a79032fedc66533caba622f0c4e7607326dfe736
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-02-01 11:46:38 +0000

    Code cleanup

--- http/contracts/client.ts
+++ http/contracts/client.ts
@@ -1,15 +1,12 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { InjectionToken } from "@angular/core";
+import { HttpErrorResponse } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { ErrorHandler } from "./error-handler";
 
 export interface Client {
   /**
    * @description Send a request to an end server with HTTP POST verb
-   * @param path Request path [[string]]
-   * @param body Request body [[object]]
-   * @param fullpath Specifies if full url is passed or not
-   * @param options Request options including HTTP headers [[object]]
+   * @param path
+   * @param body
+   * @param options
    */
   post(
     path: string,
@@ -19,17 +16,16 @@ export interface Client {
 
   /**
    * @description Send a request to an end server with HTTP GET verb
-   * @param path Request path [[string]]
-   * @param options Request options including HTTP headers [[object]]
-   * @param fullpath Specifies if full url is passed or not
+   * @param path
+   * @param options
    */
   get(path: string, options?: { [index: string]: any }): Observable<any>;
 
   /**
    * @description Send a request to an end server with HTTP PUT verb
-   * @param path Request path [[string]]
-   * @param body Request body [[object]]
-   * @param options Request options including HTTP headers [[object]]
+   * @param path
+   * @param body
+   * @param options
    * @param fullpath Specifies if full url is passed or not
    */
   put(
@@ -40,9 +36,8 @@ export interface Client {
 
   /**
    * @description Send a request to an end server with HTTP DELETE verb
-   * @param path Request path [[string]]
-   * @param options Request options including HTTP headers [[object]]
-   * @param fullpath Specifies if full url is passed or not
+   * @param path
+   * @param options
    */
   delete(path: string, options?: { [index: string]: any }): Observable<any>;
 
